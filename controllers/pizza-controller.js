5d1fc3bcff9e0ded3d36204bad6037b35ad37a29
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-02-16 17:08:12 -0600

    comment and reply functionality

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,6 +1,6 @@
-const { Pizza } = require('../models');
+    const { Pizza } = require('../models');
 
-const PizzaController = {
+    const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
@@ -13,60 +13,51 @@ const PizzaController = {
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
             console.log(err);
-            res.status(404).json(err);
+            res.sendStatus(400);
         });
     },
-    // get single pizza by ID
-    getPizzaById({ params }, res){
+
+    // get one pizza by id
+    getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
         .populate({
             path: 'comments',
             select: '-__v'
         })
         .select('-__v')
-        .then(dbPizzaData => {
-            // If no pizza is found, send 404
-            if(!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id'});
-                return;
-            }
-            res.json(dbPizzaData);
-        })
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
             console.log(err);
-            res.status(400).json(err);
+            res.sendStatus(400);
         });
     },
-    //createPizza
-    createPizza( {body}, res){
+
+    // createPizza
+    createPizza({ body }, res) {
         Pizza.create(body)
         .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => res.status(400).json(err));
+        .catch(err => res.json(err));
     },
-    //update pizza by id
-    updatePizza({ params, body }, res){
+
+    // update pizza by id
+    updatePizza({ params, body }, res) {
         Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
         .then(dbPizzaData => {
-            if(!dbPizzaData) {
-                res.status(404).json({message: 'No pizza found with this id'});
-                return;
+            if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
             }
             res.json(dbPizzaData);
         })
-        .catch(err => res.status(400).josn(err));
+        .catch(err => res.json(err));
     },
-    //delete pizza
-    deletePizza({ params }, res){
-        Pizza.findOneAndDelete({ _id: params.id })
-        .then(dbPizzaData => {
-            if(!dbPizzaData){
-                res.status(404).json({ message: 'No pizza found with this id'});
-                return;
-            }
-            res.json(dbPizzaData);
-        })
-        .catch(err => res.status(400).json(err));
-    }
-};
 
-module.exports = PizzaController;
+    // delete pizza
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
+    }
+    };
+
+    module.exports = pizzaController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -38,9 +38,8 @@ const PizzaSchema = new Schema(
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 
